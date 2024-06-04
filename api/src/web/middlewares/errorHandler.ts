28a7fbb546100d9ac12ba372f83cef9fa3733d3e
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-04 14:14:45 -0300

    feat: create error handler

---
+++ api/src/web/middlewares/errorHandler.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { InvalidArgument } from "src/domain/entities/commom/ApplicationLayerException";
+
+export default function errorHandler(
+  err: InvalidArgument,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  try {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "fail";
+    return res.status(err.statusCode).json({ errorMessage: err.message });
+  } catch (error) {
+    return res.status(500).send("internal server error");
+  }
+}
