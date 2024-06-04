eb1796fd664fd1c3fcb9fb44268946424f41a440
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-04 14:15:13 -0300

    feat: create token service

---
+++ api/src/infrastructure/services/tokenService.ts
@@ -0,0 +1,53 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { InvalidArgument } from "src/domain/entities/commom/ApplicationLayerException";
+import { ITokenService, TokenContent } from "src/domain/services/ITokenService";
+
+export class TokenService implements ITokenService {
+  constructor() {}
+  tokenValidate(token: string) {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET as string);
+    } catch (error: any) {
+      const manageableErrorMessages = ["invalid signature", "jwt expired"];
+
+      if (manageableErrorMessages.includes(error.message)) {
+        throw new InvalidArgument(error.message as string, 400);
+      }
+      throw new Error(error);
+    }
+  }
+
+  createTokens({ id, email, name }: TokenContent): {
+    accessToken: string;
+    refreshToken: string;
+  } {
+    const tokens = {
+      accessToken: this.createAccessToken({ id, email, name }),
+      refreshToken: this.createRefreshtoken({ id, email, name }),
+    };
+
+    return tokens;
+  }
+
+  decode(token: string): JwtPayload {
+    return jwt.decode(token) as JwtPayload;
+  }
+
+  private createRefreshtoken(data: {
+    name: string;
+    email: string;
+    id: string;
+  }) {
+    return jwt.sign(data, process.env.JWT_SECRET as string, {
+      expiresIn: "30d",
+    });
+  }
+
+  private createAccessToken(data: { name: string; email: string; id: string }) {
+    console.log(process.env.JWT_SECRET);
+
+    return jwt.sign(data, process.env.JWT_SECRET as string, {
+      expiresIn: "1h",
+    });
+  }
+}
