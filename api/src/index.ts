c1f3605153c80b903ff35c1de0abe7f43c4fc8c5
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-05 01:18:44 -0300

    feat: sertup typeorm dataSource initializer

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,14 +2,17 @@ import express from "express";
 import { configureDI, configureRouter } from "./web/services";
 import "dotenv/config";
 import errorHandler from "./web/middlewares/errorHandler";
+import { AppDataSource } from "./data-source";
 
-const app = express();
+AppDataSource.initialize().then(() => {
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-const diContainer = configureDI();
-configureRouter(app, diContainer);
+  const diContainer = configureDI();
+  configureRouter(app, diContainer);
 
-app.use(errorHandler);
+  app.use(errorHandler);
 
-app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000);
+});
