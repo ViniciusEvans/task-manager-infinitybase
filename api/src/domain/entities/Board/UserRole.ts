b2021eb8b0c94138904661fda332febd2e89f39f
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 01:11:04 -0300

    feat: add user role entity

--- api/src/domain/entities/Board/UserRole.ts
+++ api/src/domain/entities/Board/UserRole.ts
@@ -1,14 +1,37 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "../User/User";
 import { Board } from "./Board";
 
-export class UserRole {
-  User!: User;
-  Board!: Board;
-  UserPermissionLevel!: UserPermissionLevel;
+export enum UserPermissionLevel {
+  ADMIN = "ADMIN",
+  USER = "USER",
 }
 
-enum UserPermissionLevel {
-  OWNER = "OWNER",
-  ADMIN = "ADMIN",
-  VIEWER = "VIEWER",
+@Entity("users_role")
+export class UserRole {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @ManyToOne(() => User, (user) => user.userRole)
+  user!: User;
+
+  @ManyToOne(() => Board, (board) => board.usersRole)
+  board!: Board;
+
+  @Column({ type: "enum", enum: UserPermissionLevel })
+  userPermissionLevel!: UserPermissionLevel;
+
+  constructor(
+    user: User,
+    board: Board,
+    userPermissionLevel: UserPermissionLevel
+  ) {
+    this.user = user;
+    this.board = board;
+    this.userPermissionLevel = userPermissionLevel;
+  }
+
+  changePermission(userPermissionLevel: UserPermissionLevel) {
+    this.userPermissionLevel = userPermissionLevel;
+  }
 }
