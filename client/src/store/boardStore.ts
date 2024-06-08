5d998b8e3064a2ea1937a53128908d20b74e437c
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 02:28:06 -0300

    feat: create board store states

---
+++ client/src/store/boardStore.ts
@@ -0,0 +1,96 @@
+import { create } from 'zustand'
+
+type CreateBoardModalState = {
+    showCreateBoardModal: boolean
+    setShowCreateBoardModal: () => void
+}
+
+type User = {
+    id: string
+    name: string
+}
+
+enum EUserPermissionLevel {
+    ADMIN = 'ADMIN',
+    USER = 'USER'
+}
+
+type UserRole = {
+    userPermissionLevel: EUserPermissionLevel
+    id: string
+    user: User
+}
+
+type Attachment = {
+    id: string
+    attachmentUrl: string
+}
+
+type Task = {
+    id: string
+    title: string
+    taskStatus: TaskStatus
+    description: string
+    user: User
+    attachments: Attachment[] | { fileExt: string; fileBuffer: Buffer }[]
+}
+
+type TaskStatus = { id: string; status: string }
+
+type Board = { id: string; name: string; usersRole: UserRole[]; taskStatus: TaskStatus[]; tasks: Task[] }
+
+type BoardsState = {
+    boards: Board[]
+    setBoards: (boards: Board[]) => void
+    addBoard: (newBoard: { id: string; name: string }) => void
+}
+
+type DashboardState = {
+    board: Board
+    setDashboard: (board: Board) => void
+    addTask: (task: Task) => void
+}
+
+type CreateTaskModalState = {
+    showCreateTaskModal: boolean
+    setShowCreateTaskModal: () => void
+}
+
+type TaskFormDataState = {
+    task: Task & { boardId: string }
+    setTask: (task: Task & { boardId: string }) => void
+    resetTask: () => void
+}
+
+export const useCreateBoardModal = create<CreateBoardModalState>((set) => ({
+    showCreateBoardModal: false,
+    setShowCreateBoardModal: () => set((state) => ({ ...state, showCreateBoardModal: !state.showCreateBoardModal }))
+}))
+
+export const useBoardsStores = create<BoardsState>((set) => ({
+    boards: [],
+    addBoard: (newBoard: { id: string; name: string }) =>
+        set((state) => ({ ...state, boards: [...state.boards, { ...newBoard, tasks: [], taskStatus: [], users: [], usersRole: [] }] })),
+    setBoards: (boards: Board[]) => set((state) => ({ ...state, boards: [...boards] }))
+}))
+
+export const useDashboardStore = create<DashboardState>((set) => ({
+    board: { id: '', name: '', taskStatus: [], tasks: [], users: [], usersRole: [] },
+    setDashboard: (board: Board) => set((state) => ({ ...state, board: board })),
+    addTask: (task: Task) => set((state) => ({ ...state, board: { ...state.board, tasks: [...state.board.tasks, task] } }))
+}))
+
+export const useCreateTaskModal = create<CreateTaskModalState>((set) => ({
+    showCreateTaskModal: false,
+    setShowCreateTaskModal: () => set((state) => ({ ...state, showCreateTaskModal: !state.showCreateTaskModal }))
+}))
+
+export const useTaskFormData = create<TaskFormDataState>((set) => ({
+    task: { id: '', title: '', taskStatus: { id: '', status: '' }, description: '', user: { id: '', name: '' }, boardId: '', attachments: [] },
+    setTask: (task) => set((state) => ({ ...state, task: task })),
+    resetTask: () =>
+        set((state) => ({
+            ...state,
+            task: { id: '', title: '', taskStatus: { id: '', status: '' }, description: '', user: { id: '', name: '' }, boardId: '', attachments: [] }
+        }))
+}))
