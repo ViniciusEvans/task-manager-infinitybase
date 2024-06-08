ed30f0890088d470cc437dcadf1c8d567ff0bb37
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 01:12:31 -0300

    feat: add user entity

--- api/src/domain/entities/User/User.ts
+++ api/src/domain/entities/User/User.ts
@@ -1,4 +1,17 @@
 import { v4 as uuid } from "uuid";
+import { Board } from "../Board/Board";
+import { Task } from "../Task/Task";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { UserRole } from "../Board/UserRole";
+
+@Entity("user")
 export class User {
   constructor(name: string, email: string, password: string) {
     this.id = uuid();
@@ -6,8 +19,22 @@ export class User {
     this.password = password;
     this.email = email;
   }
+
+  @PrimaryGeneratedColumn("uuid")
   id!: string;
+
+  @Column({ type: "text" })
   name!: string;
+
+  @Column({ type: "text" })
   password!: string;
+
+  @Column({ type: "text" })
   email!: string;
+
+  @OneToMany(() => Task, (task) => task.user)
+  tasks!: Task[];
+
+  @OneToMany(() => UserRole, (userRoles)=> userRoles.user)
+  userRole!: UserRole[];
 }
