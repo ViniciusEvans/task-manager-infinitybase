78fea48547ac6e606be5f4f9cd5536e0b868449c
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 01:10:48 -0300

    feat: add board repository interface

---
+++ api/src/domain/entities/Board/IBoardRepository.ts
@@ -0,0 +1,10 @@
+import { Board } from "./Board";
+import { UserRole } from "./UserRole";
+
+export interface IBoardRepository {
+  findBoardsByUserId(userId: string): Promise<Board[]>;
+  findBoardByUserIdAndBoardId(userId: string, boardId: string): Promise<Board | null>;
+  store(board: Board): Promise<void>;
+  findBoardById(boardId: string): Promise<Board | null>;
+  removeUserRole(userRole: UserRole): Promise<void>
+}
