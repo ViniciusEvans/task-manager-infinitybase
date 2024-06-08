47a7a992955b59364cf048bf9cd837041fdfefdd
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 01:10:27 -0300

    feat: add board entity

--- api/src/domain/entities/Board/Board.ts
+++ api/src/domain/entities/Board/Board.ts
@@ -1,11 +1,55 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Task } from "../Task/Task";
+import { UserPermissionLevel, UserRole } from "./UserRole";
+import { v4 as uuid } from "uuid";
 import { User } from "../User/User";
-import { UserRole } from "./UserRole";
+import { TaskStatus } from "../Task/TaskStatus";
 
+@Entity("board")
 export class Board {
-  constructor() {}
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
 
-  Tasks!: Task[];
-  Users!: User[];
-  UsersRole!: UserRole[];
+  @Column({ type: "text" })
+  name!: string;
+
+  @OneToMany(() => Task, (task) => task.board)
+  tasks!: Task[];
+
+  @OneToMany(() => UserRole, (userRole) => userRole.board)
+  usersRole!: UserRole[];
+
+  @OneToMany(() => TaskStatus, (taskStatus) => taskStatus.board)
+  taskStatus!: TaskStatus[];
+
+  constructor(name: string) {
+    this.id = uuid();
+    this.name = name;
+  }
+
+  setOwner(user: User, board: Board) {
+    this.usersRole = [];
+    this.usersRole.push(new UserRole(user, board, UserPermissionLevel.ADMIN));
+  }
+
+  addUser(user: User, board: Board, userPermissionLevel: UserPermissionLevel) {
+    if (!this.usersRole) {
+      this.usersRole = [];
+    }
+
+    this.usersRole.push(new UserRole(user, board, userPermissionLevel));
+  }
+
+  getUser(userId: string) {
+    return this.usersRole.find((user) => user.user.id === userId);
+  }
+
+  editUserPermission(userPermissionLevel: UserPermissionLevel, userId: string) {
+    const userRole = this.getUser(userId);
+
+    if (!userRole) {
+      return;
+    }
+    userRole.changePermission(userPermissionLevel);
+  }
 }
