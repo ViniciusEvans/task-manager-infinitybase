4fda14ae87fd394e7e609e20f9241aa6bad64359
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 01:12:12 -0300

    feat: add task entity

--- api/src/domain/entities/Task/Task.ts
+++ api/src/domain/entities/Task/Task.ts
@@ -1,20 +1,51 @@
-import { Attachment } from "../Attachment/Attachment";
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { Board } from "../Board/Board";
 import { User } from "../User/User";
+import { TaskStatus } from "./TaskStatus";
+import { v4 as uuid } from "uuid";
+import { Attachment } from "./Attachment";
 
+@Entity({ name: "task" })
 export class Task {
-  constructor() {}
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
 
-  Title!: string;
-  Status!: TaskStatus;
-  Description!: string;
-  User!: User;
-  Board!: Board;
-  Attachments!: Attachment[];
-}
+  @Column({ type: "text" })
+  title!: string;
 
-export enum TaskStatus {
-  Completed = "Completed",
-  InProgress = "InProgress",
-  ToDo = "ToDo",
+  @Column({ type: "text" })
+  description!: string;
+
+  @ManyToOne(() => User, (user) => user.tasks)
+  user!: User;
+
+  @ManyToOne(() => Board)
+  board!: Board;
+
+  @ManyToOne(() => TaskStatus)
+  taskStatus!: TaskStatus;
+
+  @OneToMany(() => Attachment, (attachment) => attachment.task)
+  attachments!: Attachment[];
+
+  constructor(
+    title: string,
+    description: string,
+    user: User,
+    board: Board,
+    taskStatus: TaskStatus
+  ) {
+    this.id = uuid();
+    this.title = title;
+    this.description = description;
+    this.user = user;
+    this.board = board;
+    this.taskStatus = taskStatus;
+  }
 }
