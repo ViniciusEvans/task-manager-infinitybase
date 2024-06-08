63f02c8e8ecbc09ab384cbee8a99e317d68261dc
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 01:09:09 -0300

    feat: add api routes

---
+++ api/src/routes.ts
@@ -0,0 +1,47 @@
+// @ts-nocheck
+import { Application } from "express";
+import { AppDIContainer } from "./web/services";
+import { authorization } from "./web/middlewares/authorization";
+
+export function configureRouter(app: Application, diContainer: AppDIContainer) {
+  const {
+    usersController,
+    authenticationController,
+    boardsController,
+    tasksController,
+  } = diContainer;
+
+  app.use(authorization);
+  app
+    .route("/auth/signup")
+    .post((req, res) => usersController.signup(req, res));
+  app
+    .route("/auth/login")
+    .post((req, res) => authenticationController.login(req, res));
+  app
+    .route("/auth/refresh")
+    .post((req, res) => authenticationController.refreshAuth(req, res));
+
+  app
+    .route("/boards")
+    .get((req, res) => boardsController.getAllBoardsFromUser(req, res));
+  app
+    .route("/board")
+    .post((req, res) => boardsController.createBoard(req, res));
+  app
+    .route("/board/add-user")
+    .post((req, res) => boardsController.addUserToBoard(req, res));
+  app
+    .route("/board/remove-user")
+    .delete((req, res) => boardsController.removeUserFromBoard(req, res));
+  app
+    .route("/board/change-user-permission")
+    .put((req, res) => boardsController.changeTheUserPErmission(req, res));
+  app
+    .route("/board/:id")
+    .get((req, res) => boardsController.getBoard(req, res));
+  app
+    .route("/tasks")
+    .get((req, res) => tasksController.getTasksFromBoard(req, res));
+  app.route("/task").post((req, res) => tasksController.createTask(req, res));
+}
