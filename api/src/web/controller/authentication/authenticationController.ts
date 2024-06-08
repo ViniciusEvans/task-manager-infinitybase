da8f236d601ed5054a906ad4fca111924d9ba48b
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 01:45:10 -0300

    feat: create auth controller

--- api/src/web/controller/authentication/authenticationController.ts
+++ api/src/web/controller/authentication/authenticationController.ts
@@ -1,42 +1,33 @@
 import { InvalidArgument } from "src/domain/commom/ApplicationLayerException";
 import { AuthenticationService } from "../../../application/services/authenticationService";
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
-  async login(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { email, password } = req.body;
 
-      if (!email || !password) {
-        throw new InvalidArgument("email and password must be provided!", 400);
-      }
-      const tokens = await this.authenticationService.authenticate(
-        email,
-        password
-      );
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body;
 
-      return res.status(200).send(tokens);
-    } catch (error) {
-      next(error);
+    if (!email || !password) {
+      throw new InvalidArgument("email and password must be provided!", 400);
     }
+    const tokens = await this.authenticationService.authenticate(
+      email,
+      password
+    );
+
+    return res.status(200).send(tokens);
   }
 
-  async refreshAuth(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { refreshToken } = req.body;
-      
-      if (!refreshToken) {
-        throw new InvalidArgument("refreshToken must be provided", 400);
-      }
+  async refreshAuth(req: Request, res: Response) {
+    const { refreshToken } = req.body;
 
-      const tokens = await this.authenticationService.refreshToken(
-        refreshToken
-      );
-
-      return res.status(200).send(tokens);
-    } catch (error) {
-      next(error);
+    if (!refreshToken) {
+      throw new InvalidArgument("refreshToken must be provided", 400);
     }
+
+    const tokens = await this.authenticationService.refreshToken(refreshToken);
+
+    return res.status(200).send(tokens);
   }
 }
