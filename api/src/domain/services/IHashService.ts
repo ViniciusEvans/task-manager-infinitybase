62b920afe90a3c2c2b06cc85290c59c2c85730d5
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-08 01:13:05 -0300

    feat: add hash service interface

--- api/src/domain/services/IHashService.ts
+++ api/src/domain/services/IHashService.ts
@@ -1,4 +1,6 @@
+export type HashAlgorithms = "sha256";
 export interface IHashService {
   encrypt(password: string): Promise<string>;
   compare(toComparePassword: string, hashPassword: string): Promise<boolean>;
+  hashBinary(binary: Buffer, algorithm?: HashAlgorithms): string;
 }
