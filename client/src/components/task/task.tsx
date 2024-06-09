1ad09addad2bb8a3596c8a963b0476c70a7c168a
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-09 18:12:15 -0300

    feat: implments handle to show viewmodal

--- client/src/components/task/task.tsx
+++ client/src/components/task/task.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './style.css'
+import { useGetOneTaskStore, useViewTaskModal } from '../../store/boardStore'
 
-export const Task = ({ id, title, ownerName }: { id: string; title: string; ownerName: string }) => {
+export const Task = ({ id, title, ownerName, boardId }: { id: string; title: string; ownerName: string; boardId: string }) => {
+    const { setShowViewTaskModal } = useViewTaskModal()
+    const { setStore } = useGetOneTaskStore()
+
+    function handleClick() {
+        setStore(id, boardId)
+        setShowViewTaskModal()
+    }
+
     return (
-        <button className="task">
-            <h3 id="title">{title}</h3>
-            <span>{ownerName}</span>
-        </button>
+        <>
+            <button className="task" onClick={handleClick}>
+                <h3 id={id}>{title}</h3>
+                <span>{ownerName}</span>
+            </button>
+        </>
     )
 }
