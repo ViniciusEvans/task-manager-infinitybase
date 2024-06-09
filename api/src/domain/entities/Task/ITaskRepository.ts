a1c61a8221fa0f48872443aeb62acb36443021b8
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-09 18:09:19 -0300

    feat: add findTaskById to repository interface

--- api/src/domain/entities/Task/ITaskRepository.ts
+++ api/src/domain/entities/Task/ITaskRepository.ts
@@ -3,4 +3,5 @@ import { Task } from "./Task";
 export interface ITaskRepository {
   findTasks(boardId: string, query: string): Promise<Task[]>;
   store(task: Task): Promise<void>;
+  findTaskById(taskId: string): Promise<Task  | null>
 }
