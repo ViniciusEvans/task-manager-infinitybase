27eecba1d24e6a2d3278dfe4f4a6b025c73c6c94
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-06-10 00:06:11 -0300

    feat: handle open create form

--- client/src/components/taskBoard/taskBoard.tsx
+++ client/src/components/taskBoard/taskBoard.tsx
@@ -1,18 +1,23 @@
-import { useCreateTaskModal, useDashboardStore } from '../../store/boardStore'
+import { useTaskFormModal, useDashboardStore, useTaskFormData } from '../../store/boardStore'
 import { Task } from '../task/task'
 import React from 'react'
 import './style.css'
 
 export const TaskBoard = ({ id, name }: { id: string; name: string }) => {
     const { board } = useDashboardStore()
-    const { setShowCreateTaskModal } = useCreateTaskModal()
-
+    const { setShowTaskFormModal } = useTaskFormModal()
+    const { setIsCreate } = useTaskFormData()
     const tasks = board.tasks.filter((task) => task.taskStatus.id === id)
+
+    function handleClick() {
+        setIsCreate(true)
+        setShowTaskFormModal(true)
+    }
 
     return (
         <div className="task-board">
             <h3>{name}</h3>
-            <button className="add-task-button" onClick={setShowCreateTaskModal}>
+            <button className="add-task-button" onClick={() => handleClick()}>
                 +
             </button>
 
